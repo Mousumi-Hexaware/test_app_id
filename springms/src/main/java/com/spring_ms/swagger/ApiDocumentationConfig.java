030b48232977ad69ec344b7b837c9260c5d3328a
Mousumi-Hexaware <1000053348@hexaware.com>
2022-09-28 07:25:32 +0000

    reg push

---
+++ springms/src/main/java/com/spring_ms/swagger/ApiDocumentationConfig.java
@@ -0,0 +1,31 @@
+package com.spring_ms.swagger;
+
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.ExternalDocs;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.License;
+import io.swagger.annotations.SwaggerDefinition;
+
+@SwaggerDefinition(
+        info = @Info(
+                description = "Spring_ms Resources",
+                version = "V12.0.12",
+                title = "Spring_ms Resource API",
+                contact = @Contact(
+                   name = "Spring_ms Team", 
+                   email = "rapidx@hexaware.com", 
+                   url = "http://www.hexaware.com"
+                ),
+                license = @License(
+                   name = "Spring_ms 2.0", 
+                   url = "http://www.hexaware.com/licenses/LICENSE-2.0"
+                )
+        ),
+        consumes = {"application/json", "application/xml"},
+        produces = {"application/json", "application/xml"},
+        schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
+        externalDocs = @ExternalDocs(value = "Read This For Sure", url = "http://www.spring_ms.org")
+)
+public interface ApiDocumentationConfig {
+
+}
